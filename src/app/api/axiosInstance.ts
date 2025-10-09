1521ef81d710f3822c89214b55f63e91369ae6f3
TufanRai <thulungtufan16@gmail.com>
2025-10-09 09:53:44 +0545

    Updated the uri

--- src/app/api/axiosInstance.ts
+++ src/app/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://noteme-vudd.onrender.com/api",
   headers: {
     authorization: `BEARER ${Cookies.get("ticket")}`,
   },
